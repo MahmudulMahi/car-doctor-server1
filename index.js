49ddfba5fcdfe74b9eb20a34052773f4c8929b47
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-09 23:26:55 +0600

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8zyyzcn.mongodb.net/?retryWrites=true&w=majority`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,11 +28,31 @@ async function run() {
     await client.connect();
 
     const serviceCollection=client.db('carDoctor').collection('services')
+    const bookingCollection=client.db('carDoctor').collection('bookings')
 
+
+    // services related
     app.get('/services',async(req,res)=>{
       const cursor=serviceCollection.find()
       const result=await cursor.toArray()
       res.send(result)
+    })
+
+    app.get('/services/:id', async(req,res)=>{
+      const id =req.params.id
+      const query ={_id: new ObjectId(id)}
+
+      const options = {
+
+        projection: {  title: 1, price: 1,service_id:1 },
+      };
+      const result =await serviceCollection.findOne(query,options)
+      res.send(result)
+    })
+
+    // booking related
+    app.post('bookings',async(req,res)=>{
+      const booking=req.body;
     })
    
     await client.db("admin").command({ ping: 1 });
